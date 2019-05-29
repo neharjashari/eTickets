aa1b0c08db2e9b888e0b68d2eda6bcb6d9b8777f
neharjashari <neharjashari@gmail.com>
2019-05-29 23:15:58 +0200

    Changing the Theme

--- app/src/main/java/com/projektifiek/etickets/MainActivity.java
+++ app/src/main/java/com/projektifiek/etickets/MainActivity.java
@@ -2,6 +2,12 @@ package com.projektifiek.etickets;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +16,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+    /*MENU*/
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this, "Selected Item: " +item.getTitle(), Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()) {
+            case R.id.menu_home:
+                // do your code
+                return true;
+            case R.id.menu_user_profile:
+                // do your code
+                return true;
+            case R.id.menu_events:
+                // do your code
+                return true;
+            case R.id.menu_tickets:
+                // do your code
+                return true;
+            case R.id.menu_settings:
+                // do your code
+                return true;
+            case R.id.menu_exit_the_app:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////
 }
