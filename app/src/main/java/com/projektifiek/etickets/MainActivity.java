c8fe12fa9f476ac52df66f8270318269f501847b
neharjashari <neharjashari@gmail.com>
2019-06-02 19:46:12 +0200

    Fetching JSON data from Back End server in tha MainActivity, EventActivity & creating a template for representing Events (from Back End) into our app.

--- app/src/main/java/com/projektifiek/etickets/MainActivity.java
+++ app/src/main/java/com/projektifiek/etickets/MainActivity.java
@@ -2,57 +2,150 @@ package com.projektifiek.etickets;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView lvEvent;
+    OkHttpClient client = new OkHttpClient();
+    EventAdapter adapteri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvEvent = findViewById(R.id.lvEvent);
+        adapteri = new EventAdapter(MainActivity.this);
+        lvEvent.setAdapter(adapteri);
+
+        lvEvent.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
+        String token = "12345";
+
+        Request request = new
+                Request.Builder()
+                .url("http://192.168.179.1:8000/event/" + token)
+                .get()
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.i("exception", e.toString());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response)
+                    throws IOException {
+                if(response.isSuccessful())
+                {
+                    Log.w("JSON: ", "Successful json response.");
+
+                    String strJsonResponse =
+                            response.body().string();
+
+                    Log.w("JSON: ", strJsonResponse);
+
+                    try {
+                        JSONArray objJsonArray =
+                                new JSONArray(strJsonResponse);
+                        for(int i=0;i<objJsonArray.length();i++)
+                        {
+                            JSONObject jsonObject =
+                                    objJsonArray.getJSONObject(i);
+                            int id = jsonObject.getInt("id");
+                            String title = jsonObject.getString("title");
+                            String author = jsonObject.getString("author");
+                            String dateCreated = jsonObject.getString("date_created");
+                            String content = jsonObject.getString("content");
+                            String photoUrl = jsonObject.getString("photo");
+//                            photot = photot.substring(0,10);
+
+                            Event objEvent = new Event(id,title,author,dateCreated,content,photoUrl);
+                            adapteri.data.add(objEvent);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    finally {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                adapteri.notifyDataSetChanged();
+                            }
+                        });
+                    }
+                }
+            }
+        });
+
     }
 
 
 
-    /*MENU*/
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Toast.makeText(this, "Selected Item: " +item.getTitle(), Toast.LENGTH_SHORT).show();
-        switch (item.getItemId()) {
-            case R.id.menu_home:
-                // do your code
-                return true;
-            case R.id.menu_user_profile:
-                // do your code
-                return true;
-            case R.id.menu_events:
-                // do your code
-                return true;
-            case R.id.menu_tickets:
-                // do your code
-                return true;
-            case R.id.menu_settings:
-                // do your code
-                return true;
-            case R.id.menu_exit_the_app:
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+//    /*MENU*/
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        Toast.makeText(this, "Selected Item: " +item.getTitle(), Toast.LENGTH_SHORT).show();
+//        switch (item.getItemId()) {
+//            case R.id.menu_home:
+//                // do your code
+//                return true;
+//            case R.id.menu_user_profile:
+//                // do your code
+//                return true;
+//            case R.id.menu_events:
+//                // do your code
+//                return true;
+//            case R.id.menu_tickets:
+//                // do your code
+//                return true;
+//            case R.id.menu_settings:
+//                // do your code
+//                return true;
+//            case R.id.menu_exit_the_app:
+//                finish();
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
 
 
     /////////////////////////////////////////////////////////////////////////////////
