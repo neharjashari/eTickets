caea4bafd45dbb663b53487a4b77aeac05745ab6
neharjashari <37405052+neharjashari@users.noreply.github.com>
2019-06-10 12:18:15 +0200

    Changing some of the style.

--- app/src/main/java/com/projektifiek/etickets/OpeningScreen.java
+++ app/src/main/java/com/projektifiek/etickets/OpeningScreen.java
@@ -4,21 +4,26 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class OpeningScreen extends AppCompatActivity {
 
-    private static int SPLASH_SCREEN_DELAY = 3000;
+    private static int SPLASH_SCREEN_DELAY = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening_screen);
 
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 // Executed after timer is finished (Opens MainActivity)
-                Intent intent = new Intent(OpeningScreen.this, MainActivity.class);
+                Intent intent = new Intent(OpeningScreen.this, LoginActivity.class);
+                Animation mLoadAnimation = AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_in);
+                mLoadAnimation.setDuration(3000);
                 startActivity(intent);
 
                 // Kills this Activity
